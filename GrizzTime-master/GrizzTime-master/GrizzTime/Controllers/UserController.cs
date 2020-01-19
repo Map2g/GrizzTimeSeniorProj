da9fed7b9c198ffa16d62e7f7f736783dd0452f4
danjsalar <47092571+danjsalar@users.noreply.github.com>
2020-01-18 20:53:39 -0500

    SSL Added to website

--- GrizzTime-master/GrizzTime-master/GrizzTime/Controllers/UserController.cs
+++ GrizzTime-master/GrizzTime-master/GrizzTime/Controllers/UserController.cs
@@ -158,6 +158,10 @@ namespace GrizzTime.Controllers
                 return View();
             }
         }
+
+
+    
+
         //[NonAction]
         //public void SendVerificationEMail(string email)
         //{
@@ -174,7 +178,7 @@ namespace GrizzTime.Controllers
         //    var smtp = new SmtpClient
         //    {
         //        Host = "smtp.gmail.com",
-        //    Port = 587,
+        //        Port = 587,
         //        EnableSsl = true,
         //        DeliveryMethod = SmtpDeliveryMethod.Network,
         //        UseDefaultCredentials = false,
