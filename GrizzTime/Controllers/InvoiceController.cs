c3faa12abbf1b1e960965212208af72cf3c0815c
Mel Palmer <melpalmer421@gmail.com>
2020-03-13 14:56:52 -0400

    Beginning invoices

---
+++ GrizzTime/Controllers/InvoiceController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrizzTime.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public ActionResult EmployeeInvoice(int eid)
+        {
+            return View();
+        }
+
+        public ActionResult ProjectInvoice(int pid)
+        {
+            return View();
+        }
+    }
+}
